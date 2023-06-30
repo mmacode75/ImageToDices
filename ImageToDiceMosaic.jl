0a9cf61562bf78f245edc45d024d1087a69bdda4
mmacode75 <manuel.martin.arillo@gmail.com>
2023-06-30 11:40:47 +0200

    modified:   ImageToDiceMosaic.jl

--- ImageToDiceMosaic.jl
+++ ImageToDiceMosaic.jl
@@ -10,14 +10,7 @@ imageAux=channelview(Image);
 result=Int16.(round.(mean(imageAux,dims=1)*6,RoundNearestTiesUp))[1,:,:];
 reversed=reverse(dices); #black over white
 #reversed=dices #white over black
-function interchange(data)
-    if(data==0)
-        return reversed[1];
-    end
-    return reversed[data];
-end
-
-text=map(interchange,result)
+text=map(data->data==0 ? reversed[1] : reversed[data],result)
 writedlm("./dicesvalues.txt",result)
 writedlm("./chifuyu.txt",text,""); #open txt and change to bold and a strong font for better view ^_^ in some screens its difficult to see :(
 
